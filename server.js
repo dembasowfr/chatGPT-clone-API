8022d557f7a5beed19e508ad4acdd5dd2abb10eb
dembasow98 <dastech1998@gmail.com>
2023-01-30 03:06:00 +0300

    Made a little bit of changes on the server.js file.

--- server.js
+++ server.js
@@ -6,17 +6,22 @@ require("dotenv").config({ path: "./config.env" });
 // get driver connection
 const dbo = require("./database/connection");
 
+//Connection to MongoDB
+
+dbo.connectToServer(function (err) {
+  if (err) console.error(err);
+});
+
+
 const port = process.env.PORT || 8000;
+
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/chatbot"));
 
  
 app.listen(port, () => {
+  dbo.getDb();
   // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
-
-  });
   console.log(`Server is running on port: ${port}`);
 });
